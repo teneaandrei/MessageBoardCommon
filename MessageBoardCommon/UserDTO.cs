9023caa1216ee526b9e572fb02fab496b0ad3679
Andrei Tenea <andrei.tenea93@gmail.com>
2017-01-21 19:06:42 +0200

    Minor modifications

--- MessageBoardCommon/UserDTO.cs
+++ MessageBoardCommon/UserDTO.cs
@@ -7,52 +7,41 @@ using System.Threading.Tasks;
 
 namespace MessageBoardDTO
 {
-    [DataContract]
+    
     public partial class UserDTO
     {
-        [DataMember]
         public int UserID { get; set; }
-
-        [DataMember]
+        
         public string FirstName { get; set; }
 
-        [DataMember]
         public string LastName { get; set; }
 
-        [DataMember]
         public string Username { get; set; }
 
-        [DataMember]
         public string PasswordHash { get; set; }
 
-        [DataMember]
+        public string PasswordSalt { get; set; }
+
         public string Country { get; set; }
 
-        [DataMember]
         public string City { get; set; }
 
-        [DataMember]
         public string Function { get; set; }
 
-        [DataMember]
         public byte[] ProfileImage { get; set; }
 
-        [DataMember]
         public bool IsAdministrator { get; set; }
 
-        [DataMember]
         public bool IsActive { get; set; }
 
-        [DataMember]
         public System.DateTime AccountCreationDate { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
 
-        [DataMember]
         public virtual ICollection<CommentDTO> tblComments { get; set; }
+
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
 
-        [DataMember]
         public virtual ICollection<PostDTO> tblPosts { get; set; }
 
     }
