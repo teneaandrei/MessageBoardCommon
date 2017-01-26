0352b0c0b5fcae43e1b06c2b8f0d67fce3bb493c
Andrei Tenea <andrei.tenea93@gmail.com>
2017-01-27 00:02:55 +0200

    Adding logger class

---
+++ MessageBoardCommon/Logger.cs
@@ -0,0 +1,27 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MessageBoardCommon
+{
+    public static class Logger
+    {
+        private static readonly ILog _log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        static Logger()
+        {
+            log4net.Config.XmlConfigurator.Configure();
+        }
+
+        public static void Error(string format, params object[] args)
+        {
+            _log.Error(string.Format(format, args));
+        }
+    }
+}
