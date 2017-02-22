92dc853c001bd44d1dc597ae3d72c15a9231f318
Andrei Tenea <andrei.tenea93@gmail.com>
2017-02-23 00:11:21 +0200

    LoggerConfig and Informations method

--- MessageBoardCommon/Logger.cs
+++ MessageBoardCommon/Logger.cs
@@ -12,8 +12,9 @@ namespace MessageBoardCommon
 {
     public static class Logger
     {
-        private static readonly ILog _log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-
+        //private static readonly ILog _log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private static readonly ILog _logError = log4net.LogManager.GetLogger("Error");
+        private static readonly ILog _logWrongPassword = log4net.LogManager.GetLogger("Summary");
         static Logger()
         {
             log4net.Config.XmlConfigurator.Configure();
@@ -21,7 +22,11 @@ namespace MessageBoardCommon
 
         public static void Error(string format, params object[] args)
         {
-            _log.Error(string.Format(format, args));
+            _logError.Error(string.Format(format, args));
+        }
+        public static void Information(string format, params object[] args)
+        {
+            _logWrongPassword.Info(string.Format(format, args));
         }
     }
 }
