d2abc16ec32d0eb2af3e405b2f2ff08325e75679
Andrei Tenea <andrei.tenea93@gmail.com>
2017-03-12 23:03:18 +0200

    Updating BaseDTO

--- MessageBoardCommon/BaseDTO.cs
+++ MessageBoardCommon/BaseDTO.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MessageBoardCommon
 {
+    [DataContract]
     public class BaseDTO
     {
+        [DataMember]
         public bool IsError { get; set; }
+        [DataMember]
         public bool ErrorMessage { get; set; }
     }
 }
