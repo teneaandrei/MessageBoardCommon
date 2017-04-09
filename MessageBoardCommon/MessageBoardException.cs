1e792b3d346bee52e1250fa311d28a3292b21167
Andrei Tenea <andrei.tenea93@gmail.com>
2017-04-09 18:16:03 +0300

    Adding MessageBoardException

--- MessageBoardCommon/MessageBoardException.cs
+++ MessageBoardCommon/MessageBoardException.cs
@@ -1,29 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Data.SqlClient;
+using System.ServiceModel;
 
 namespace MessageBoardCommon
 {
     public class MessageBoardException : Exception
     {
+        private Exception _ex;
+
         public MessageBoardException()
             : base() { }
 
         public MessageBoardException(string message)
             : base(message)
         {
-            
         }
 
         public MessageBoardException(string format, params object[] args)
             : base(string.Format(format, args)) { }
 
         public MessageBoardException(string message, Exception innerException)
-            : base(message, innerException) { }
+            : base(message, innerException)
+        {
+            _ex = innerException;
+        }
 
         public MessageBoardException(string format, Exception innerException, params object[] args)
-            : base(string.Format(format, args), innerException) { }
+            : base(string.Format(format, args), innerException)
+        {
+            _ex = innerException;
+        }
+
+
+        public void WriteErrorMessage()
+        {
+            if (_ex is TimeoutException)
+            {
+                XtraMessageBox.Show(Constants.ERR_TIMEOUT_EX);
+            }
+            if (_ex is EndpointNotFoundException)
+            {
+                XtraMessageBox.Show(Constants.ExceptionService);
+            }
+            if (_ex is NullReferenceException)
+            {
+                XtraMessageBox.Show(Constants.ExceptionNullObjReference);
+            }
+            if (_ex is SqlException)
+            {
+                XtraMessageBox.Show(_ex.Message);
+            }
+        }
     }
 }
